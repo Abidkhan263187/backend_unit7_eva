62b2ef746815e9fa0f528a9066d06c3b6ca0c283
abidkhan263187 <abidkhan263187@gmail.com>
2023-07-01 12:56:35 +0530

    blog

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ const {todoCrudRouter}=require('./RouteController/todoCrud')
 
 const app=express();
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with the domain of your React application
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:3000', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   }));
 
-app.get('/',(req,res)=>{
+// app.get('/',(req,res)=>{
 
-    res.json("getting all the todos from DB")
-})
+//     res.json("getting all the todos from DB")
+// })
 
+ app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
 app.use('/user',todoUserRouter)
 
